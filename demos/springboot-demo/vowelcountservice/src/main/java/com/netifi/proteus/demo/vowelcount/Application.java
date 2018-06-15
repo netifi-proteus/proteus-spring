4f17f7203107bcfaec2d5fbf7285b0240bccd6e6
gregwhitaker <gwhitake@gmail.com>
2018-06-15 13:47:01 -0700

    Doing work

--- demos/springboot-demo/vowelcountservice/src/main/java/com/netifi/proteus/demo/vowelcount/Application.java
+++ demos/springboot-demo/vowelcountservice/src/main/java/com/netifi/proteus/demo/vowelcount/Application.java
@@ -15,9 +15,13 @@
  */
 package com.netifi.proteus.demo.vowelcount;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
     public static void main(String... args) {
-
+        SpringApplication.run(Application.class, args);
     }
 }
